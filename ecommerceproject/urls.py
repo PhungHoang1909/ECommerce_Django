ac3426c4c32af68db6167ab7d48e1d743e860c34
phung hoang <lnphunghoang190903@gmail.com>
2024-05-11 14:07:58 +0700

    Add Product to Cart

--- ecommerceproject/urls.py
+++ ecommerceproject/urls.py
@@ -20,9 +20,12 @@ from django.urls import path
 from ecommerceapp.views import frontpage, shop
 from product.views import product
 
+from Cart.views import add_to_cart
+
 urlpatterns = [
     path("", frontpage, name="frontpage"),
     path("shop/", shop, name="shop"),
     path("shop/<slug:slug>/", product, name="product"),
+    path("add_to_cart/<int:product_id>/", add_to_cart, name="add_to_cart"),
     path("admin/", admin.site.urls),
 ]
